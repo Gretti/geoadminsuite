86b90413f2d787135737e2d14b4753f5b6d0ba7c
nicky666 <nicky666@d78974e2-b545-0410-a236-1b700048ef5c>
2008-12-16 10:52:06 +0000

    licence added

--- src/java/org/geogurus/tools/sql/ConPool.java
+++ src/java/org/geogurus/tools/sql/ConPool.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2003-2008  Gretti N'Guessan, Nicolas Ribot
+ *
+ * This file is part of GeoAdminSuite
+ *
+ * GeoAdminSuite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GeoAdminSuite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GeoAdminSuite.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.geogurus.tools.sql;
 
 import java.sql.Connection;
