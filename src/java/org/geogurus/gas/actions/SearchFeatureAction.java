b0c7e341939fc71a87d29a088fa36ee3b3b493c8
gretti.nguessan <gretti.nguessan@d78974e2-b545-0410-a236-1b700048ef5c>
2008-12-18 15:08:18 +0000

    reprojection of given point if map projection differs from gc projection

--- src/java/org/geogurus/gas/actions/SearchFeatureAction.java
+++ src/java/org/geogurus/gas/actions/SearchFeatureAction.java
@@ -38,10 +38,22 @@ import java.io.Writer;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.geogurus.data.DataAccess;
 import org.geogurus.data.DataAccessType;
 import org.geogurus.gas.objects.UserMapBean;
 import org.geogurus.mapserver.objects.Map;
+import org.geogurus.mapserver.objects.Projection;
+import org.geotools.geometry.GeneralDirectPosition;
+import org.geotools.referencing.CRS;
+import org.geotools.referencing.ReferencingFactoryFinder;
+import org.opengis.geometry.DirectPosition;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.CoordinateOperation;
+import org.opengis.referencing.operation.CoordinateOperationFactory;
+import org.opengis.referencing.operation.MathTransform;
 
 /**
  *
@@ -77,6 +89,33 @@ public class SearchFeatureAction extends org.apache.struts.action.Action {
                 gc.getDatasourceType() != DataAccessType.WMS) {
             //We also need units of current map
             UserMapBean umb = (UserMapBean) session.getAttribute(ObjectKeys.USER_MAP_BEAN);
+            //Check projection of gc and mapfile => reproject given point and
+            //recalculate tolerance if necessary
+            Projection p = umb.getMapfile().getProjection();
+            String param = (String) p.getAttributes().get(0);
+            String mapEpsg = param.substring(param.lastIndexOf(":") + 1, param.lastIndexOf("\""));
+            String gcEpsg = String.valueOf(gc.getSrid());
+            if (!gcEpsg.equals(mapEpsg)) {
+                //Reproject given point
+                CoordinateReferenceSystem crsSrc = null;
+                CoordinateReferenceSystem crsDest = ReferencingFactoryFinder.getCRSAuthorityFactory("EPSG", null).createCoordinateReferenceSystem(gcEpsg);
+                if (!mapEpsg.equals("900913")) {
+                    crsSrc = ReferencingFactoryFinder.getCRSAuthorityFactory("EPSG", null).createCoordinateReferenceSystem(mapEpsg);
+                } else {
+                    try {
+                        crsSrc = CRS.parseWKT("PROJCS[\"Google Mercator\",GEOGCS[\"WGS 84\",DATUM[\"World Geodetic System 1984\",SPHEROID[\"WGS 84\",6378137.0,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0.0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.017453292519943295],AXIS[\"Geodetic latitude\",NORTH],AXIS[\"Geodetic longitude\",EAST],AUTHORITY[\"EPSG\",\"4326\"]],PROJECTION[\"Mercator_1SP\"],PARAMETER[\"semi_minor\",6378137.0],PARAMETER[\"latitude_of_origin\",0.0],PARAMETER[\"central_meridian\",0.0],PARAMETER[\"scale_factor\",1.0],PARAMETER[\"false_easting\",0.0],PARAMETER[\"false_northing\",0.0],UNIT[\"m\",1.0],AXIS[\"Easting\",EAST],AXIS[\"Northing\",NORTH],AUTHORITY[\"EPSG\",\"900913\"]],EXTENSION[\"PROJ4\",\"+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs\"]]");
+                    } catch (FactoryException ex) {
+                        Logger.getLogger(SearchFeatureAction.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                CoordinateOperationFactory coFactory = ReferencingFactoryFinder.getCoordinateOperationFactory(null);
+                CoordinateOperation op = coFactory.createOperation(crsSrc, crsDest);
+                MathTransform trans = op.getMathTransform();
+                DirectPosition ll = new GeneralDirectPosition(Double.valueOf(lon).doubleValue(), Double.valueOf(lat).doubleValue());
+                ll = trans.transform(ll, null);
+                lon = String.valueOf(ll.getCoordinate()[0]);
+                lat = String.valueOf(ll.getCoordinate()[1]);
+            }
             byte units = umb.getMapfile().getUnits();
             String toleranceUnit;
             switch (units) {
@@ -104,8 +143,6 @@ public class SearchFeatureAction extends org.apache.struts.action.Action {
             }
 
             //Builds featuresource using geotools
-            //TODO: check projection of gc => reproject given point and
-            //recalculate tolerance if necessary
             SimpleFeature feature = DatasourceBuilder.getFeature(gc, lon, lat, tolerance, toleranceUnit);
 
             //Finds feature of current layer
