754de163437ca6e16cfd1f5ecd6109b724d93b05
nicky666 <nicky666@d78974e2-b545-0410-a236-1b700048ef5c>
2008-12-21 19:36:16 +0000

    Added code to delete published folders

--- src/java/org/geogurus/web/GOFileCleaner.java
+++ src/java/org/geogurus/web/GOFileCleaner.java
@@ -87,26 +87,41 @@ public final class GOFileCleaner extends HttpServlet {
             logger.severe("cannot load gas.properties file");
             e.printStackTrace();
         }
-
         // the temp mapfile deletion delay, based upon the initialisation parameter:
         // tmp maps will be deleted each <tempMapsDeletionDelay>
         String periodS = p.getProperty("tempMapsDeletionPeriod");
         // the temp mapfile deletion time range, based upon the initialisation parameter:
         // tmp maps will be deleted if older than <tempMapsDeletionTimeRange>
         String timeRange = p.getProperty("tempMapsDeletionTimeRange");
-        
+
         if (periodS != null && timeRange != null) {
             try {
                 // the time to wait before cleaning mapfiles: converts it in milliseconds
                 long period = new Long(periodS).longValue() * 1000;
                 long timerange = new Long(timeRange).longValue() * 1000;
                 String path = getServletContext().getRealPath("").replace('\\','/') + "/msFiles/tmpMaps/*.*";
-                FileDeletionTimerTask fd = new FileDeletionTimerTask(path, timerange, FileDeletionTimerTask.INTERVAL_BEFORE);
+                FileDeletionTimerTask fd = new FileDeletionTimerTask(path, timerange, FileDeletionTimerTask.INTERVAL_BEFORE, false);
                 // this timer is a daemon
                 timer = new Timer(true);
                 timer.schedule(fd, new Date(), period);
             } catch (NumberFormatException nfe) {nfe.printStackTrace();}
         }
+        periodS = p.getProperty("publishedDeletionPeriod");
+        timeRange = p.getProperty("publishedDeletionTimeRange");
+
+        if (periodS != null && timeRange != null) {
+            try {
+                // the time to wait before cleaning mapfiles: converts it in milliseconds
+                long period = new Long(periodS).longValue() * 1000;
+                long timerange = new Long(timeRange).longValue() * 1000;
+                String path = getServletContext().getRealPath("").replace('\\','/') + "/published/*.*";
+                FileDeletionTimerTask fd = new FileDeletionTimerTask(path, timerange, FileDeletionTimerTask.INTERVAL_BEFORE, true);
+                // this timer is a daemon
+                timer = new Timer(true);
+                timer.schedule(fd, new Date(), period);
+            } catch (NumberFormatException nfe) {nfe.printStackTrace();}
+        }
+
         
         // also loads all symbologies into the SymbologyListBean object and stores it in application context
         String symFile = p.getProperty("GAS_SYMBOL_FILE");
