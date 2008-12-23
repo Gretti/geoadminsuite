6df778a35d30fce6ea77bef0a14445904a9f9337
gretti.nguessan <gretti.nguessan@d78974e2-b545-0410-a236-1b700048ef5c>
2008-12-23 00:19:51 +0000

    still reproj 900913 pb

--- trunk/src/java/org/geogurus/gas/actions/SearchFeatureAction.java
+++ trunk/src/java/org/geogurus/gas/actions/SearchFeatureAction.java
@@ -98,6 +98,7 @@ public class SearchFeatureAction extends org.apache.struts.action.Action {
             //Check projection of gc and mapfile => reproject given point and
             //recalculate tolerance if necessary
             Projection p = umb.getMapfile().getProjection();
+            byte units = umb.getMapfile().getUnits();
             String param = (String) p.getAttributes().get(0);
             String mapEpsg = param.substring(param.lastIndexOf(":") + 1, param.lastIndexOf("\""));
             String gcEpsg = String.valueOf(gc.getSrid());
@@ -107,22 +108,41 @@ public class SearchFeatureAction extends org.apache.struts.action.Action {
                 CoordinateReferenceSystem crsDest = ReferencingFactoryFinder.getCRSAuthorityFactory("EPSG", null).createCoordinateReferenceSystem(gcEpsg);
                 if (!mapEpsg.equals("900913")) {
                     crsSrc = ReferencingFactoryFinder.getCRSAuthorityFactory("EPSG", null).createCoordinateReferenceSystem(mapEpsg);
-                } else {
+                    CoordinateOperationFactory coFactory = ReferencingFactoryFinder.getCoordinateOperationFactory(null);
+                    CoordinateOperation op = coFactory.createOperation(crsSrc, crsDest);
+                    MathTransform trans = op.getMathTransform();
+                    DirectPosition ll = new GeneralDirectPosition(Double.valueOf(lon).doubleValue(), Double.valueOf(lat).doubleValue());
+                    ll = trans.transform(ll, null);
+                    lon = String.valueOf(ll.getCoordinate()[0]);
+                    lat = String.valueOf(ll.getCoordinate()[1]);
+                } else if (!gcEpsg.equals("4326")) {
                     try {
                         crsSrc = CRS.parseWKT("PROJCS[\"Google Mercator\",GEOGCS[\"WGS 84\",DATUM[\"World Geodetic System 1984\",SPHEROID[\"WGS 84\",6378137.0,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0.0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.017453292519943295],AXIS[\"Geodetic latitude\",NORTH],AXIS[\"Geodetic longitude\",EAST],AUTHORITY[\"EPSG\",\"4326\"]],PROJECTION[\"Mercator_1SP\"],PARAMETER[\"semi_minor\",6378137.0],PARAMETER[\"latitude_of_origin\",0.0],PARAMETER[\"central_meridian\",0.0],PARAMETER[\"scale_factor\",1.0],PARAMETER[\"false_easting\",0.0],PARAMETER[\"false_northing\",0.0],UNIT[\"m\",1.0],AXIS[\"Easting\",EAST],AXIS[\"Northing\",NORTH],AUTHORITY[\"EPSG\",\"900913\"]],EXTENSION[\"PROJ4\",\"+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs\"]]");
+                        CoordinateOperationFactory coFactory = ReferencingFactoryFinder.getCoordinateOperationFactory(null);
+                        CoordinateOperation op = coFactory.createOperation(crsSrc, crsDest);
+                        MathTransform trans = op.getMathTransform();
+                        DirectPosition ll = new GeneralDirectPosition(Double.valueOf(lon).doubleValue(), Double.valueOf(lat).doubleValue());
+                        ll = trans.transform(ll, null);
+                        lon = String.valueOf(ll.getCoordinate()[0]);
+                        lat = String.valueOf(ll.getCoordinate()[1]);
                     } catch (FactoryException ex) {
                         Logger.getLogger(SearchFeatureAction.class.getName()).log(Level.SEVERE, null, ex);
                     }
+                } else {
+                    double originShift = 2 * Math.PI * 6378137 / 2;
+                    double x = Double.valueOf(lon).doubleValue();
+                    double y = Double.valueOf(lat).doubleValue();
+                    double tol = Double.valueOf(tolerance).doubleValue();
+                    x = (x / originShift) * 180;
+                    y = (y / originShift) * 180;
+                    tol = (tol / originShift) * 180;
+                    y = 180 / Math.PI * (2 * Math.atan(Math.exp(y * Math.PI / 180.0)) - Math.PI / 2.0);
+                    tol = 180 / Math.PI * (2 * Math.atan(Math.exp(tol * Math.PI / 180.0)) - Math.PI / 2.0);
+                    lon = String.valueOf(x);
+                    lat = String.valueOf(y);
+                    tolerance = String.valueOf(tol);
                 }
-                CoordinateOperationFactory coFactory = ReferencingFactoryFinder.getCoordinateOperationFactory(null);
-                CoordinateOperation op = coFactory.createOperation(crsSrc, crsDest);
-                MathTransform trans = op.getMathTransform();
-                DirectPosition ll = new GeneralDirectPosition(Double.valueOf(lon).doubleValue(), Double.valueOf(lat).doubleValue());
-                ll = trans.transform(ll, null);
-                lon = String.valueOf(ll.getCoordinate()[0]);
-                lat = String.valueOf(ll.getCoordinate()[1]);
             }
-            byte units = umb.getMapfile().getUnits();
             String toleranceUnit;
             switch (units) {
                 case Map.INCHES:
@@ -140,9 +160,6 @@ public class SearchFeatureAction extends org.apache.struts.action.Action {
                     break;
                 case Map.DD:
                     toleranceUnit = "kilometers";
-                    /*in this case tolerance is given in decimal degrees so we must
-                    approximately convert tolerance in kilometers*/
-                    //tolerance = String.valueOf(Double.parseDouble(tolerance) * 110);
                     break;
                 default:
                     toleranceUnit = "kilometers";
@@ -175,7 +192,7 @@ public class SearchFeatureAction extends org.apache.struts.action.Action {
                             feature.getDefaultGeometryProperty().getType().getName().getLocalPart())) {
                         String name = feature.getFeatureType().getType(o).getName().getLocalPart();
                         Object value = feature.getAttribute(name);
-                        if (value instanceof Number && !(value instanceof Integer)) {
+                        if (value instanceof Number && !(value instanceof Integer) && !(value instanceof Long)) {
                             try {
                                 double v = Double.parseDouble(String.valueOf(value));
                                 value = String.valueOf(f.format(v));
