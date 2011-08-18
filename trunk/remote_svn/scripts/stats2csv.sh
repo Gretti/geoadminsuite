9d30e20a0c686aa2baebaebff57ab781f63b0544
nicky666@gmail.com <nicky666@gmail.com@d78974e2-b545-0410-a236-1b700048ef5c>
2011-08-18 15:10:57 +0000

    script d'export des stats en CSV

---
+++ trunk/remote_svn/scripts/stats2csv.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# exports stats into CSV
+
+CURD=` date +'%Y%m%d_%H_%M'`
+psql bva -c "copy (select code_uu, proc_start, proc_end, nb_trajets_initiaux, sum_lgtron2006, prc_long, extract('epoch' from (proc_end - proc_start)) as duree_en_sec from stats where status_ok)  to '/mnt/data/results/latest_stats_s2_$CURD.csv' with (format CSV, header true)"
+
+if [ $? -eq 0 ]; then
+    echo "statistiques exportées dans le fichiers: /mnt/data/results/latest_stats_s2_$CURD.csv'"
+else 
+    echo "Export CSV en erreur"
+fi
