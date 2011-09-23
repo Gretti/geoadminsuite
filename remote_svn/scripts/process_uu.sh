5827eb5d7d6eaa7655e9ea8d5fa7afbe85fdaa1f
maatthieu1984@gmail.com <maatthieu1984@gmail.com@d78974e2-b545-0410-a236-1b700048ef5c>
2011-09-23 07:45:16 +0000

    toto

--- remote_svn/scripts/process_uu.sh
+++ remote_svn/scripts/process_uu.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ################################################################################
 # Script permettant de lancer le recalage des trajets en masse (UU par UU)
 # , en se basant sur un fichier texte contenant la liste des UU a traiter
@@ -29,6 +29,9 @@ DBCONN="-d bva -p 5432"
 # Chemin dans lequel se trouvent les RAR contenant les trajets
 UU_PATH=/mnt/data/UU10-100
 
+# Chemin dans lequel se trouvent les donnees de 2010
+UU_PATH_2010=/mnt/data/2010
+
 # Chemin ou sont copiés les données issues du traitement
 OUT_PATH=/mnt/data/results
 
@@ -46,7 +49,7 @@ UU100_ATTR_LIST=(
 "MODETR: String" 
 "MODEPRINC: String" 
 "IDDEPL: String" 
-"CODE_GS: String" 
+"CODE_GS: String"
 "POSTE: String" 
 "ID: String" 
 "LGTRON: Real" 
@@ -66,7 +69,8 @@ __UU100_ATTR_LIST=(
 "ID: String" 
 "LGTRON: Real" 
 "PID: Real" 
-"POIDS: Real")	
+"POIDS: Real"
+)	
 
 ###################################### Fonctions ###############################
 
@@ -153,31 +157,51 @@ if [[ ! -d $OUT_PATH/ko ]] ; then
     echo "creation du sous repertoire de sortie: $OUT_PATH/ko"
     mkdir $OUT_PATH/ko
 fi
-
                                                        
-for UUCODE in `cat $1`; do
+for UUCODE_FILE in `cat $1`; do
+	# On ne veut que les 5 premiers caractères de chaque ligne -> code UU uniquement
+	UUCODE=`echo $UUCODE_FILE | cut -c1-5`
+	# On ne veut que le type du fichier compresse 'zip' ou 'rar' et on force le lower case
+	UUCODE_TYPE=`echo $UUCODE_FILE | rev | cut -c1-3 | rev | tr -s '[:upper:]' '[:lower:]'`
+	# On ne veut que le nom du fichier sans l'extension
+	UUCODE_BASENAME=`echo $UUCODE_FILE | rev | cut -c5- | rev`
+
 	echo ""
 	echo "_________________Traitement de l'unite urbaine: $UUCODE _____________"
-	
+	echo "Fichier de l'unite urbaine : $UUCODE_FILE"
+	echo "Format de compression du fichier : $UUCODE_TYPE"
+
 	# insertion en base de cette UU, pour controle.
 	psql $DBCONN -c "delete from stats where code_uu='${UUCODE}'"
 	psql $DBCONN -c "insert into stats(code_uu, proc_start) values ('${UUCODE}', now())"
 	
-	# copie et traitement du rar
-	echo "copie du rar: ${UUCODE}.rar"
-	cp $UU_PATH/${UUCODE}.rar $TMP_DIR/
-	cd $TMP_DIR && unrar x -inul ${UUCODE}.rar	
-	
+	# suppression preventive d'un fichier precedent
+	rm -rf $TMP_DIR/$UUCODE*
+	# copie et traitement du fichier compresse
+	echo "copie du fichier: ${UUCODE_FILE}"
+	cp $UU_PATH/${UUCODE_FILE} $TMP_DIR/
+	# ${UUCODE}.rar devient ${UUCODE_FILE}
+	# decompression du fichier suivant son type
+	if [[ $UUCODE_TYPE == "rar" ]] ; then
+		CMD_DECOMPRESS="unrar x -inul ${UUCODE_FILE}"
+	else
+		CMD_DECOMPRESS="unzip ${UUCODE_FILE} -d $UUCODE_BASENAME"
+	fi
+	cd $TMP_DIR && $CMD_DECOMPRESS
+	echo "Decompression des trajets 2006 : ${UUCODE_FILE}"
+
 	# no filename expansion in [[ ]] so use old [ ] instead
-	if [ ! -f $UUCODE/[Rr]esultats/trajetssimul.shp ] || [ ! -f $UUCODE/[Rr]esultats/trajetssimul.dbf ] || [ ! -f $UUCODE/[Rr]esultats/trajetssimul.shx ] ; then
-		echo "Les fichiers shapefile des trajets (${UUCODE}/resultats/trajetssimul.shp, .dbf, .shx) n'existent pas ou n'ont pas le bon nom."
+	#if [ ! -f $UUCODE/[Rr]esultats/trajetssimul.shp ] || [ ! -f $UUCODE/[Rr]esultats/trajetssimul.dbf ] || [ ! -f $UUCODE/[Rr]esultats/trajetssimul.shx ] ; then
+	if [ ! -f $UUCODE*/trajetssimul.shp ] || [ ! -f $UUCODE*/trajetssimul.dbf ] || [ ! -f $UUCODE*/trajetssimul.shx ] ; then
+		echo "Les fichiers shapefile des trajets (${UUCODE}.../trajetssimul.shp, .dbf, .shx) n'existent pas ou n'ont pas le bon nom."
 		echo "UU $UUCODE non traitée"
 		# insertion dans la table de stats 
-		psql $DBCONN -c "update stats set proc_end=now(), status_ok=false, description='Fichiers de trajets non trouves dans l archive ${UUCODE}.rar' where code_uu='${UUCODE}'"
-		rm -rf ${UUCODE}.rar && rm -rf ${UUCODE} 
+		psql $DBCONN -c "update stats set proc_end=now(), status_ok=false, description='Fichiers de trajets non trouves dans l archive ${UUCODE_FILE}' where code_uu='${UUCODE}'"
+		ls -al ${UUCODE}*
+		rm -rf ${UUCODE}*
 	else
 		# suite du traitement normal: les fichiers sont présents. Formats valides pour les attributs ?
-		testattr ${UUCODE}/[Rr]esultats/trajetssimul.shp
+		testattr ${UUCODE}*/trajetssimul.shp
 		
 		if [[ $? -eq 1 ]] ; then
 			echo "Attributs incorrects. La table de stat contient les raisons de l'invalidité..."
@@ -185,89 +209,140 @@ for UUCODE in `cat $1`; do
 			#nettoyage de l'archive.
 			echo "nettoyage du rar..."
 			cd $TMP_DIR
-			rm -rf ${UUCODE}.rar && rm -rf ${UUCODE}  
+			rm -rf ${UUCODE}*
 		else
-			# tous les attributs sont OK, on balance le traitement:
-			
-			# insertion en base de cette UU en cours
-			echo "insertion en base de l'uu en cours: ${UUCODE}"
-			
-			echo "Lancement du traitement SQL..."
-			cd $CUR_DIR
-			# lancement sur le bon rep: ameliorer pour ne pas tenir compte de la casse.
-			if [ -d $TMP_DIR/$UUCODE/resultats ] ; then
-				#echo "sh process_trajets.sh $TMP_DIR/$UUCODE/resultats/trajetssimul.shp $2"
-				sh process_trajets.sh $TMP_DIR/$UUCODE/resultats/trajetssimul.shp $2
+			# Trajectoire 2006 ok passons au trajet 2010
+			echo "Verification des trajets 2006 de l'UU ${UUCODE}........ OK"
+
+			# Unrar du fichier trajectoire de 2010
+			UUCODE_FILE_2010=$UU_PATH_2010/${UUCODE}/${UUCODE}.rar
+			TMP_DIR_2010=$TMP_DIR/2010_${UUCODE}
+			rm -rf $TMP_DIR_2010 && mkdir -p $TMP_DIR_2010
+			echo "Fichier archive contenant les trajets 2010 : ${UUCODE_FILE_2010}"
+			unrar x -inul ${UUCODE_FILE_2010} $TMP_DIR_2010
+			echo "Decompression des trajets 2010 dans le dossier ${TMP_DIR_2010}"
+
+			# Fichier shapefile 2010
+# 			SHAPE_FILE_2010="$TMP_DIR_2010/Trajetssimul$UUCODE.shp"
+			SHAPE_FILE_2010="$TMP_DIR_2010/Trajetssimul${UUCODE}_Merge.shp"
+			echo "Fichier Shapefile 2010 de l'uu : "$SHAPE_FILE_2010
+
+			#if [ ! -f $SHAPE_FILE_2010 ] || [ ! -f $TMP_DIR_2010/Trajetssimul${UUCODE}.dbf ] || [ ! -f $TMP_DIR_2010/Trajetssimul${UUCODE}.shx ] ; then
+			if [ ! -f $SHAPE_FILE_2010 ] || [ ! -f $TMP_DIR_2010/Trajetssimul${UUCODE}_Merge.dbf ] || [ ! -f $TMP_DIR_2010/Trajetssimul${UUCODE}_Merge.shx ] ; then
+				echo "Les fichiers shapefile des trajets 2010 ($SHAPE_FILE_2010, .dbf, .shx) n'existent pas ou n'ont pas le bon nom."
+				echo "UU $UUCODE non traitée"
+				# insertion dans la table de stats 
+				psql $DBCONN -c "update stats set proc_end=now(), status_ok=false, description='Fichiers de trajets 2010 non trouves dans l archive ${UUCODE_FILE}' where code_uu='${UUCODE}'"
+				ls -al $TMP_DIR_2010
+				rm -rf $TMP_DIR_2010 
 			else
-				#echo "sh process_trajets.sh $TMP_DIR/$UUCODE/Resultats/trajetssimul.shp $2"
-				sh process_trajets.sh $TMP_DIR/$UUCODE/Resultats/trajetssimul.shp $2
-			fi
-			STATUS_CODE=$?
-			
-			if [[ $STATUS_CODE -eq 0 ]] ; then
-				DESC_ERREUR=
-				DB_STATUS_OK=true
-				
-            elif [[ $STATUS_CODE -eq 100 ]]; then
-			    DESC_ERREUR="Donnees de trajets incorrectes: impossible de reconstruire des trajets complets sous forme de MULTILINESTRING"
-				DB_STATUS_OK=false
-				
-			elif [[ $STATUS_CODE -eq 200 ]] ; then
-			    DESC_ERREUR="process_trajet en erreur: chargement des trajets impossible:  $TMP_DIR/$UUCODE/resultats/trajetssimul.shp"
-				DB_STATUS_OK=false
-			else 
-			    DESC_ERREUR="process_trajet en erreur: erreur inattendue. (code=$STATUS_CODE)"
-				DB_STATUS_OK=false
-			fi
-			#insertion en base de la fin du traitement
-			psql $DBCONN -c "update stats set proc_end=now(), status_ok=${DB_STATUS_OK}, description='${DESC_ERREUR}' where code_uu='${UUCODE}'"
-			
-			#nettoyage de l'archive.
-			echo "nettoyage du rar..."
-			cd $TMP_DIR
-			rm -rf ${UUCODE}.rar && rm -rf ${UUCODE}  
-			testcmdstatus $?
-			
-			#zip et renommage des fichiers de sortie uniquement si process OK
-			if [[ $DB_STATUS_OK == "true" ]] ; then
-				echo "zip du resultat..."
-				cd $2
-				zip -q uu_${UUCODE}.zip trajet_*.* *.csv
-				if [[ $? == 0 ]] ; then
-					echo "...................OK"
-					echo "effacement des fichiers de sortie..."
-					rm -f trajet_*.* && rm -f *.csv
+				# suite du traitement normal: les fichiers sont présents. Formats valides pour les attributs ?
+# 				testattr ${SHAPE_FILE_2010}
+				echo "Tout va bien"
+
+				if [[ $? -eq 1 ]] ; then
+					echo "Attributs incorrects pour les trajets 2010. La table de stat contient les raisons de l'invalidité..."
+					echo "arret du traitement pour l'UU $UUCODE."
+					#nettoyage de l'archive.
+					echo "nettoyage du rar..."
+					rm -rf $TMP_DIR_2010
+				else
+					# tous les attributs sont OK, on balance le traitement:
+
+					# Fichier contenant les ponderations
+					PONDERATION_FILE="$UU_PATH_2010/${UUCODE}/base_individus_${UUCODE}.txt"
+					echo "Fichier de ponderation de l'uu : "$PONDERATION_FILE
+					# Fichier shapefile
+					SHAPE_FILE="$TMP_DIR/$UUCODE*/trajetssimul.shp"
+					echo "Fichier Shapefile de l'uu : "$SHAPE_FILE
+
+					# insertion en base de cette UU en cours
+					echo "insertion en base de l'uu en cours: ${UUCODE}"
+					
+					echo "Lancement du traitement SQL..."
+					cd $CUR_DIR
+					sh process_trajets.sh $SHAPE_FILE $2 $PONDERATION_FILE $SHAPE_FILE_2010
+		#			# lancement sur le bon rep: ameliorer pour ne pas tenir compte de la casse.
+		#			if [ -d $TMP_DIR/$UUCODE/resultats ] ; then
+		#				#echo "sh process_trajets.sh $TMP_DIR/$UUCODE/resultats/trajetssimul.shp $2"
+		#				sh process_trajets.sh $TMP_DIR/$UUCODE*/resultats/trajetssimul.shp $2
+		#			else
+		#				#echo "sh process_trajets.sh $TMP_DIR/$UUCODE/Resultats/trajetssimul.shp $2"
+		#				sh process_trajets.sh $TMP_DIR/$UUCODE*/Resultats/trajetssimul.shp $2
+		#			fi
+					STATUS_CODE=$?
+					
+					if [[ $STATUS_CODE -eq 0 ]] ; then
+						DESC_ERREUR=
+						DB_STATUS_OK=true
+					elif [[ $STATUS_CODE -eq 100 ]]; then
+					    DESC_ERREUR="Donnees de trajets incorrectes: impossible de reconstruire des trajets complets sous forme de MULTILINESTRING"
+						DB_STATUS_OK=false
+					elif [[ $STATUS_CODE -eq 200 ]] ; then
+					    DESC_ERREUR="process_trajet en erreur: chargement des trajets impossible: $SHAPE_FILE"
+						DB_STATUS_OK=false
+					elif [[ $STATUS_CODE -eq 300 ]] ; then
+					    DESC_ERREUR="process_trajet en erreur: chargement de la ponderation impossible: $PONDERATION_FILE"
+						DB_STATUS_OK=false
+					else 
+					    DESC_ERREUR="process_trajet en erreur: erreur inattendue. (code=$STATUS_CODE)"
+						DB_STATUS_OK=false
+					fi
+					#insertion en base de la fin du traitement
+					psql $DBCONN -c "update stats set proc_end=now(), status_ok=${DB_STATUS_OK}, description='${DESC_ERREUR}' where code_uu='${UUCODE}'"
+					
+					#nettoyage de l'archive.
+					echo "nettoyage du rar..."
+					# Archive trajet 2006
+					cd $TMP_DIR
+					rm -rf ${UUCODE}*
 					testcmdstatus $?
-					echo $CMD_STATUS
+					# Archive trajet 2010
+					rm -rf $TMP_DIR_2010
+					testcmdstatus $?
 					
-					# copie du resultat vers l'hote de destination, en fonction du pourcentage de longueur:
-					# si > 95, copie dans rep ok, sinon dans rep ko.
-                    # utilisation de la comparaison en SQL car shell sait pas faire
-					PRC_LONG=`psql $DBCONN -At -c "select prc_long > 95 from stats where code_uu='$UUCODE'"`
-					if [ $PRC_LONG = 't' ] ; then
-					    echo "deplacement du resultat dans le rep OK :)"
-					    mv uu_${UUCODE}.zip $OUT_PATH/ok
-					else 
-					    echo "mauvaises stat, deplacement du resultat dans le rep KO :(("
-					    mv uu_${UUCODE}.zip $OUT_PATH/ko
+					#zip et renommage des fichiers de sortie uniquement si process OK
+					if [[ $DB_STATUS_OK == "true" ]] ; then
+						echo "zip du resultat..."
+						cd $2
+						zip -q uu_${UUCODE}.zip trajet_*.* *.csv
+						if [[ $? == 0 ]] ; then
+							echo "...................OK"
+							echo "effacement des fichiers de sortie..."
+							rm -f trajet_*.* && rm -f *.csv
+							testcmdstatus $?
+							echo $CMD_STATUS
+							
+							# copie du resultat vers l'hote de destination, en fonction du pourcentage de longueur:
+							# si > 95, copie dans rep ok, sinon dans rep ko.
+							# utilisation de la comparaison en SQL car shell sait pas faire
+							PRC_LONG=`psql $DBCONN -At -c "select prc_long > 95 from stats where code_uu='$UUCODE'"`
+							if [ $PRC_LONG = 't' ] ; then
+							    echo "deplacement du resultat dans le rep OK :)"
+							    mv uu_${UUCODE}.zip $OUT_PATH/ok
+							else 
+							    echo "mauvaises stat, deplacement du resultat dans le rep KO :(("
+							    mv uu_${UUCODE}.zip $OUT_PATH/ko
+							fi
+							
+							if [[ $? == 0 ]] ; then
+								echo "...................OK"
+								echo "effacement du zip de resultat..."
+								rm -f uu_${UUCODE}.zip
+								testcmdstatus $?
+								echo $CMD_STATUS
+							else 
+								echo "...................KO"
+							fi
+						else 
+							echo "...................KO"
+						fi
 					fi
-					
-					if [[ $? == 0 ]] ; then
-						echo "...................OK"
-						echo "effacement du zip de resultat..."
-						rm -f uu_${UUCODE}.zip
-						testcmdstatus $?
-						echo $CMD_STATUS
-					else 
-						echo "...................KO"
-					fi
-				else 
-					echo "...................KO"
+					# message de fin de traitement
+					END="$(date +%s)"
+					echo "temps de traitement UU $UUCODE: $(expr $END - $T0) s."
 				fi
 			fi
-			# message de fin de traitement
-			END="$(date +%s)"
-			echo "temps de traitement UU $UUCODE: $(expr $END - $T0) s."
 		fi
 	fi
 done
