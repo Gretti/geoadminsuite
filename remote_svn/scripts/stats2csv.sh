3652cfcf88106d5e82e187c9c6ac2d6bec363f32
maatthieu1984@gmail.com <maatthieu1984@gmail.com@d78974e2-b545-0410-a236-1b700048ef5c>
2011-09-29 15:43:29 +0000

    tt

--- remote_svn/scripts/stats2csv.sh
+++ remote_svn/scripts/stats2csv.sh
@@ -3,10 +3,10 @@
 # exports stats into CSV
 
 CURD=` date +'%Y%m%d_%H_%M'`
-psql bva -c "copy (select code_uu, proc_start, proc_end, nb_trajets_initiaux, sum_lgtron2006, prc_long, extract('epoch' from (proc_end - proc_start)) as duree_en_sec from stats where status_ok)  to '/mnt/data/results/latest_stats_s2_$CURD.csv' with (format CSV, header true)"
+psql bva -c "copy (select code_uu, proc_start, proc_end, nb_trajets_initiaux, sum_lgtron2006, prc_long, extract('epoch' from (proc_end - proc_start)) as duree_en_sec from stats where status_ok)  to '/mnt/data/results/latest_stats_s1_$CURD.csv' with (format CSV, header true)"
 
 if [ $? -eq 0 ]; then
-    echo "statistiques a la date du $(date) exportées dans le fichiers: /mnt/data/results/latest_stats_s2_$CURD.csv"
+    echo "statistiques a la date du $(date) exportées dans le fichiers: /mnt/data/results/latest_stats_s1_$CURD.csv"
 else 
     echo "Export CSV en erreur"
 fi
