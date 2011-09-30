21cd3aa899130a3b34912d97bd101a0cde5a67ab
maatthieu1984@gmail.com <maatthieu1984@gmail.com@d78974e2-b545-0410-a236-1b700048ef5c>
2011-09-30 09:41:24 +0000

    ...

--- trunk/remote_svn/scripts/ident_new.sh
+++ trunk/remote_svn/scripts/ident_new.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#shp2pgsql remote_svn/data/base_tirage_ident_new.dbf ident_new | psql -d bva
+shp2pgsql remote_svn/data/base_tirage_ident_new.dbf ident_new | psql -d bva
 
 echo "create index ident_new_aresimuler on ident_new(aresimuler);" | psql -d bva
 echo "create index ident_new_id_new on ident_new(id_new);" | psql -d bva
