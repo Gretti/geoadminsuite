4fcd1331c5719cd1123832e687283351a6bde9be
maatthieu1984@gmail.com <maatthieu1984@gmail.com@d78974e2-b545-0410-a236-1b700048ef5c>
2011-10-21 09:20:57 +0000

    update process_merge script

--- remote_svn/scripts/process_merge_shp.sh
+++ remote_svn/scripts/process_merge_shp.sh
@@ -7,14 +7,14 @@ cd /tmp/${UU_CODE}_Merge
 
 unrar x -o+ /mnt/data/UU-2010/UU_100/${UU_CODE}/${UU_CODE}.rar
 
-shp2psgsql -iISDd -W LATIN1 -s 25572 -geom trajetssimul${UU_CODE}p1.shp trajet_2010 |psql -d bva -p 5432
-shp2psgsql -iISDd -W LATIN1 -s 25572 -geom Trajetssimul${UU_CODE}p1.shp trajet_2010 |psql -d bva -p 5432
+shp2pgsql -iISDd -W LATIN1 -s 25572 -geom trajetssimul${UU_CODE}p1.shp trajet_2010 |psql -d bva -p 5432
+shp2pgsql -iISDd -W LATIN1 -s 25572 -geom Trajetssimul${UU_CODE}p1.shp trajet_2010 |psql -d bva -p 5432
 
-shp2psgsql -iISDa -W LATIN1 -s 25572 -geom trajetssimul${UU_CODE}p2.shp trajet_2010 |psql -d bva -p 5432
-shp2psgsql -iISDa -W LATIN1 -s 25572 -geom Trajetssimul${UU_CODE}p2.shp trajet_2010 |psql -d bva -p 5432
+shp2pgsql -iISDa -W LATIN1 -s 25572 -geom trajetssimul${UU_CODE}p2.shp trajet_2010 |psql -d bva -p 5432
+shp2pgsql -iISDa -W LATIN1 -s 25572 -geom Trajetssimul${UU_CODE}p2.shp trajet_2010 |psql -d bva -p 5432
 
-shp2psgsql -iISDa -W LATIN1 -s 25572 -geom trajetssimul${UU_CODE}p3.shp trajet_2010 |psql -d bva -p 5432
-shp2psgsql -iISDa -W LATIN1 -s 25572 -geom Trajetssimul${UU_CODE}p3.shp trajet_2010 |psql -d bva -p 5432
+shp2pgsql -iISDa -W LATIN1 -s 25572 -geom trajetssimul${UU_CODE}p3.shp trajet_2010 |psql -d bva -p 5432
+shp2pgsql -iISDa -W LATIN1 -s 25572 -geom Trajetssimul${UU_CODE}p3.shp trajet_2010 |psql -d bva -p 5432
 
 cd $OLDPWD
 
