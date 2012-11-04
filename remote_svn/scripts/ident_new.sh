17dfae85be1193e2a530cc40b47ff00d4052d4ad
nicky666@gmail.com <nicky666@gmail.com@d78974e2-b545-0410-a236-1b700048ef5c>
2012-11-04 19:18:54 +0000

    New place for java code stored in svn

--- remote_svn/scripts/ident_new.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-shp2pgsql remote_svn/data/base_tirage_ident_new.dbf ident_new | psql -d bva
-
-echo "create index ident_new_aresimuler on ident_new(aresimuler);" | psql -d bva
-echo "create index ident_new_id_new on ident_new(id_new);" | psql -d bva
-echo "create index ident_new_uu99 on ident_new(uu99);" | psql -d bva
--- web/scripts/refexportfiles/resources/images/vista/toolbar/MapFish/server/python/mapfish/util.py
+++
@@ -1,8 +0,0 @@
-from paste.script import templates
-
-class MapFishTemplate(templates.Template):
-    egg_plugins = ['MapFish']
-    summary = 'Template for creating a basic MapFish project'
-    required_templates = ['pylons']
-    _template_dir = 'templates/project'
-    overwrite = True
