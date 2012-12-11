2b7e49e1bf3c7cf026b1c7ff33ac9b2d6bced68d
nicky666@gmail.com <nicky666@gmail.com@d78974e2-b545-0410-a236-1b700048ef5c>
2012-12-11 15:19:37 +0000

    correction de la structure de la table trajet_2009_tmp: elle doit etre la meme que celle de trajet_2009

--- trunk/remote_svn/SQL/calage_trajet_nvq_ok.sql
+++ trunk/remote_svn/SQL/calage_trajet_nvq_ok.sql
@@ -2,14 +2,23 @@
 -- Extraction des trajets pour lesquels TOUS les codes NVQ sont valides
 drop table if exists trajet_2009_ok;
 create table trajet_2009_ok as (
-	with tmp(tgid, idtraj, voirie_gid, link_id,  shape_length, voirie_geom, count) as (
-		select distinct t.gid, t.idtraj, v.gid,  v.link_id, v.shape_length, v.geom, count(ordre) OVER (partition by t.idtraj)
+	with tmp(tgid, numdepl, sens, modetr, modeprinc, numtrajet, poste, id, pid, poids,
+	         idtraj, voirie_gid, link_id,  shape_length, voirie_geom, count) as (
+		select distinct t.gid, numdepl, sens, modetr, modeprinc, numtrajet, poste, id, pid, poids,
+		t.idtraj, v.gid,  v.link_id, v.shape_length, v.geom, count(ordre) OVER (partition by t.idtraj)
 		from trajet t, voirie_work v
 		where t.code_gs::int = v.link_id
 		and v.idtraj = t.idtraj
 	)
-	select t.idtraj, tmp.voirie_gid as gid, -1 as node_id, t.ordre, tmp.link_id, 
-         tmp.shape_length as lgtron2009, t.lgtron2006, tmp.voirie_geom as geom
+	select t.idtraj, 
+	        t.numdepl, t.sens, t.modetr, t.modeprinc, t.numtrajet, t.poste, t.id, t.pid, t.poids,
+	       tmp.voirie_gid as gid, 
+	       -1 as node_id, 
+	       t.ordre, 
+	       tmp.link_id, 
+           tmp.shape_length as lgtron2009, 
+           t.lgtron2006, 
+           tmp.voirie_geom as geom
 	from tmp, trajet t, trajet_full tf
 	where tmp.idtraj = tf.idtraj
 	and t.gid = tmp.tgid
