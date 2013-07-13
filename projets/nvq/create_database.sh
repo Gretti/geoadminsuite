3ab33d38ed7c66694a867c65d1b972c0b13b4d98
nicky666@gmail.com <nicky666@gmail.com@d78974e2-b545-0410-a236-1b700048ef5c>
2013-07-13 13:23:36 +0000

    New script files

--- projets/nvq/create_database.sh
+++ projets/nvq/create_database.sh
@@ -10,3 +10,31 @@
 #  Configuration variable:
 # Edit the following variable to match target environment
 ################################################################################
+
+# *DBCON = -U $DBUSER -h $DBHOST -p $DBPORT
+
+
+echo "Dropping the database $DBNAME if exists..."
+$PGBIN/dropdb --if-exists  $DBCON  $DBNAME
+echo "Database $DBANE dropped."
+echo
+
+"Creating database $DBNAME..."
+$PGBIN/createdb  $DBCON -E UTF-8 -l c $DBNAME
+echo "Database $DBANE created."
+echo
+
+"installing Postgis through Extension..."
+$PGBIN/psql  $DBCON - c "create extension postgis" -d $DBNAME
+OUT=$?
+
+if [ $OUT -eq 0 ];then
+    echo "Postgis extension created"
+else
+    echo "Unable to create postgis extension, aborting."
+    exit 1
+fi
+
+echo "Done with database creation"
+echo
+
