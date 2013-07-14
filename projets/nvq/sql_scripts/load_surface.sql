bd7ab91cf12ff28acb8de8372263089d19a15414
nicky666@gmail.com <nicky666@gmail.com@d78974e2-b545-0410-a236-1b700048ef5c>
2013-07-14 13:55:11 +0000

    first test with shell scripts

--- projets/nvq/sql_scripts/load_surface.sql
+++ projets/nvq/sql_scripts/load_surface.sql
@@ -9,16 +9,16 @@ create sequence surf_seq;
 -- 
 drop table if exists surface; 
 create table surface as (
-    select nextval('surf_seq') as gid, 1::int as polygon_class, 5::int as zlevel, polygon_id, polygon_nm, nm_langcd, st_transform(the_geom, 3857) as the_geom 
+    select nextval('surf_seq') as gid, 3::int as polygon_class, 5::int as zlevel, polygon_id, polygon_nm, nm_langcd, st_transform(the_geom, 3857) as the_geom 
     from adminbndy2
     UNION ALL
-    select nextval('surf_seq') as gid, 4::int as zlevel, polygon_id, polygon_nm, nm_langcd, st_transform(the_geom, 3857) as the_geom 
+    select nextval('surf_seq') as gid, 4::int as polygon_class, 4::int as zlevel, polygon_id, polygon_nm, nm_langcd, st_transform(the_geom, 3857) as the_geom 
     from adminbndy3
     UNION ALL
-    select nextval('surf_seq') as gid, 3::int as zlevel, polygon_id, polygon_nm, nm_langcd, st_transform(the_geom, 3857) as the_geom 
+    select nextval('surf_seq') as gid, 5::int as polygon_class, 3::int as zlevel, polygon_id, polygon_nm, nm_langcd, st_transform(the_geom, 3857) as the_geom 
     from adminbndy4
     UNION ALL
-    select nextval('surf_seq') as gid, 2::int as zlevel, polygon_id, polygon_nm, nm_langcd, st_transform(the_geom, 3857) as the_geom 
+    select nextval('surf_seq') as gid, 6::int as polygon_class, 2::int as zlevel, polygon_id, polygon_nm, nm_langcd, st_transform(the_geom, 3857) as the_geom 
     from adminbndy5
 );
 
@@ -30,19 +30,20 @@ drop table adminbndy5;
 
 -- add constraints to the table
 alter table surface add primary key (gid);
+ALTER TABLE surface ADD CONSTRAINT fk_surface_polygon_class FOREIGN KEY (polygon_class) REFERENCES class_code (id);
 
 -- indexes
 create index surface_the_geom_gist on surface using gist(the_geom);
 
 -- table doc: generates comments for objects
-comment on surface is 'surfacic objects from Navstreets. Used to draw map background';
-comment on surface.gid is 'unique id, PK';
-comment on surface.zlevel is 'z-level of the object. one z-level by polygon type';
-comment on surface.polygon_class is 'Class of the polygon. FK on class_code table';
-comment on surface.polygon_id is 'ID of the polygon. From navstreet tables';
-comment on surface.polygon_nm is 'name of the polygon. From navstreet tables';
-comment on surface.nm_langcd is 'language code for the polygon. From navstreet tables';
-comment on surface.the_geom is 'Multipolygon geometry. SRID=3857';
+comment on table surface is 'surfacic objects from Navstreets. Used to draw map background';
+comment on column surface.gid is 'unique id, PK';
+comment on column surface.zlevel is 'z-level of the object. one z-level by polygon type';
+comment on column surface.polygon_class is 'Class of the polygon. FK on class_code table';
+comment on column surface.polygon_id is 'ID of the polygon. From navstreet tables';
+comment on column surface.polygon_nm is 'name of the polygon. From navstreet tables';
+comment on column surface.nm_langcd is 'language code for the polygon. From navstreet tables';
+comment on column surface.the_geom is 'Multipolygon geometry. SRID=3857';
 
 -- generates simplified versions of the surface table
 
