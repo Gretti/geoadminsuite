878493d7ac433b6626965d75f47886288b101b2c
nicky666@gmail.com <nicky666@gmail.com@d78974e2-b545-0410-a236-1b700048ef5c>
2013-09-10 13:06:41 +0000

    To save the previous code, new jetty server introduced

--- code/java/PgAdmin2MapServer/src/pgAdmin2MapServer/Pg2MS.java
+++ code/java/PgAdmin2MapServer/src/pgAdmin2MapServer/Pg2MS.java
@@ -14,13 +14,16 @@ import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
 import org.java_websocket.WebSocket;
 import pgAdmin2MapServer.client.ElementalHttpPost;
+import pgAdmin2MapServer.model.Map;
 import pgAdmin2MapServer.model.Mapfile;
 import pgAdmin2MapServer.server.ElementalHttpServer;
 import pgAdmin2MapServer.server.ElementalWebSocketServer;
 import pgAdmin2MapServer.server.RequestManager;
 
 /**
- * TODO: real logging...
+ * Main class: starts the server if needed or send new params to running server
+ * to refresh client map
+ * TODO: real logging..., paths config
  *
  * @author nicolas
  */
@@ -31,8 +34,12 @@ public class Pg2MS {
      * The Relative path to zip file containing HTML resources (Map, JS et al.)
      */
     // for easier debug
-    public static final String HTML_RESOURCES_PATH = "/Users/nicolas/code/java/PgAdmin2MapServer/html";
-    public static final boolean READ_FROM_ZIP = true;
+    public static final boolean READ_FROM_ZIP = false;
+    
+    /**
+     * True to use GeoJSON format for the vector layers, false to use MapServer layers
+     */
+    public static final boolean USE_FORAMT_GEOJSON = true;
     /**
      * The Swing frame displaying program logs and allowing geographic files
      * drag'n'drop (not yet available)
@@ -43,26 +50,24 @@ public class Pg2MS {
      */
     public static int serverPort = 9472;
     /**
-     * The WebSocket server port this server listent to
+     * The WebSocket server port this server listen to
      */
     public static int webSocketServerPort = 8887;
     public static boolean debug = true;
-    public static boolean debugNetwork = false;
+    public static boolean debugNetwork = true;
     public static String mapfileName = "pgadmin_viewer.map";
     // TODO: get it from config or smart guess
-    public static String tmpDir = "/tmp";
-    public static String mapserverExe = System.getProperty("os.name").contains("Windows") ? "mapserv.exe" : "mapserv";
-    public static String mapfileUrl = "http://localhost/cgi-bin/mapserv?map=" + tmpDir + File.separator + mapfileName;
+    public static File tmpDir = null;
+    public static String mapserverExe = null;
+    public static String mapfileUrl = null;
     public static ElementalWebSocketServer wsServer = null;
+    
     // The full path to the HTML resources this program uses to display the map
     // will be computed live when the program is run
     public static File resourceFile = null;
-    /**
-     * The tmp file where MapServer can write image TODO: find correct folder
-     * according to plateform
-     */
-    public static String docRoot = tmpDir + File.separator;
     private static String todel = "";
+    /** The map containing the layers */
+    public static Map map;
 
     /**
      * Thread for test WebSocket ticker, sending client a message each 30 sec
@@ -70,24 +75,24 @@ public class Pg2MS {
     private static class WSTestRunner implements Runnable {
 
         public void run() {
-            Pg2MS.log("Ticker thread started...");
+            Pg2MSJetty.log("Ticker thread started...");
 
             while (true) {
                 if (Pg2MS.wsServer != null) {
                     Date d = new Date();
                     for (WebSocket ws : Pg2MS.wsServer.connections()) {
                         if (ws.isOpen()) {
-                            Pg2MS.log("sending ticker to client: " + ws.getRemoteSocketAddress().toString());
+                            Pg2MSJetty.log("sending ticker to client: " + ws.getRemoteSocketAddress().toString());
                             ws.send("server time: " + d.toString());
                         }
                     }
                 } else {
-                    Pg2MS.log("Ticker: no webSocket to send to.");
+                    Pg2MSJetty.log("Ticker: no webSocket to send to.");
                 }
                 try {
                     Thread.sleep(30000);
                 } catch (InterruptedException ie) {
-                    Pg2MS.log("Ticker thread error" + ie.toString());
+                    Pg2MSJetty.log("Ticker thread error" + ie.toString());
                 }
             }
         }
@@ -109,7 +114,7 @@ public class Pg2MS {
      *
      * @param msg
      */
-    public static void log(String msg) {
+    public static void nolog(String msg) {
         if (debug) {
             if (Pg2MS.fileDroper != null && Pg2MS.fileDroper.jTextArea1 != null) {
                 Pg2MS.fileDroper.jTextArea1.setText(Pg2MS.fileDroper.jTextArea1.getText() + "\n" + msg);
@@ -121,19 +126,33 @@ public class Pg2MS {
 
     /**
      * Performs some internal initialization
+     * Config object is available if init called from startServer().
      */
     public static void init() {
-        // init the full path to the HTML resources
-        // System.getProperty("user.dir") is not robust enough on plateforms to use it:
-        System.out.println(Pg2MS.class.getClass().getProtectionDomain().getCodeSource().getLocation());
-        try {
-            //s = new File(".").getAbsolutePath();
-        } catch (Exception e) {
-            
+        
+        System.out.println(System.getProperty("java.io.tmpdir"));
+        // init the full path to the HTML resources according to target plateform:
+        // on windows, html.zip resource is built from bindir
+        // on *nix plateform, uses the user.dir variable to get the working directory
+        // tmp dir must be mapserver readable, so force it.
+        if (System.getProperty("os.name").contains("Windows")) {
+            Pg2MS.resourceFile = new File(Config.getInstance().binDir + File.separator 
+                    + "plugins.d"+ File.separator + "lib"
+                    , "html.zip");
+            Pg2MS.tmpDir = new File("c:\\windows\\temp");
+           
+        } else {
+            if (!Pg2MS.READ_FROM_ZIP) {
+                Pg2MS.resourceFile = new File(System.getProperty("user.dir") + "/html");
+            } else {
+                String sub = "lib" + File.separator + "html.zip";
+                Pg2MS.resourceFile = new File(System.getProperty("user.dir"), sub);
+            }
+            Pg2MS.tmpDir = new File("/tmp");
         }
-        String sub = "lib" + File.separator + "html.zip";
-        Pg2MS.resourceFile = new File(System.getProperty("user.dir"), sub);
-        Pg2MS.log("HTML resources in: " + Pg2MS.resourceFile.getAbsolutePath());
+        Pg2MSJetty.log("HTML resources in: " + Pg2MS.resourceFile.getAbsolutePath());
+        Pg2MS.mapserverExe = System.getProperty("os.name").contains("Windows") ? "mapserv.exe" : "mapserv";
+        Pg2MS.mapfileUrl = "http://localhost/cgi-bin/mapserv?map=" + tmpDir + File.separator + mapfileName;
     }
 
     /**
@@ -148,14 +167,14 @@ public class Pg2MS {
         // internal initialisation
         init();
         // Starts Web server
-        Thread t = new ElementalHttpServer.RequestListenerThread(Pg2MS.serverPort, docRoot);
+        Thread t = new ElementalHttpServer.RequestListenerThread(Pg2MS.serverPort);
         t.setDaemon(false);
         t.start();
 
         // starts Web Socket server
         Pg2MS.wsServer = new ElementalWebSocketServer(webSocketServerPort);
         Pg2MS.wsServer.start();
-        Pg2MS.log("WebSocket Server started on port: " + Pg2MS.wsServer.getPort());
+        Pg2MSJetty.log("WebSocket Server started on port: " + Pg2MS.wsServer.getPort());
 
         // starts test ticker 
         //Thread th = new Thread(new WSTestRunner());
@@ -172,8 +191,8 @@ public class Pg2MS {
     }
 
     /**
-     * Loads layers and launch browser. Should be called in the main gui onload
-     * event
+     * Loads layers and launch browser. It is called in the main gui onload
+     * event. 
      *
      * @param openBrowser true to open a browser with a MAP url (client
      * initialization) false to send new MapConfig JSON through webSOcket
@@ -181,15 +200,16 @@ public class Pg2MS {
      * @throws Exception if an error occured
      */
     public static void loadLayers(boolean openBrowser) throws Exception {
-        String m = Mapfile.write();
+        map = new Map();
+        //String m = Mapfile.write();
 
         // opens the normal page: the client, fetching the config, should see its empty 
         //  => no geo to display
-        if (m != null && !m.isEmpty()) {
-            Pg2MS.log("mapfile written: " + m);
-        } else {
-            Pg2MS.log("No mapfile to write: no geo data from database");
-        }
+//        if (m != null && !m.isEmpty()) {
+//            Pg2MSJetty.log("mapfile written: " + m);
+//        } else {
+//            Pg2MSJetty.log("No mapfile to write: no geo data from database");
+//        }
 
         if (openBrowser) {
             //String action = RequestManager.REQUEST_FILE + "?fileName=" + URLEncoder.encode("/resources/ol.html", "UTF-8");
@@ -198,17 +218,17 @@ public class Pg2MS {
                     .replace("action", action);
 
             // Open GUI: a client browser ;)
-            Pg2MS.log("Launching default browser with URL: " + serverUrl);
+            Pg2MSJetty.log("Launching default browser with URL: " + serverUrl);
             Desktop.getDesktop().browse(URI.create(serverUrl));
 
-            Pg2MS.log("GUI launched with config: " + Config.getInstance().toString());
-            Pg2MS.log(todel);
+            Pg2MSJetty.log("GUI launched with config: " + Config.getInstance().toString());
+            Pg2MSJetty.log(todel);
         } else {
             // send new layer config to client webSocket
             for (WebSocket ws : Pg2MS.wsServer.connections()) {
                 if (ws.isOpen()) {
-                    Pg2MS.log("sending new MapConfig JSON to client: " + ws.getRemoteSocketAddress().toString());
-                    ws.send(Mapfile.getMapConfigJson(false));
+                    Pg2MSJetty.log("sending new MapConfig JSON to client: " + ws.getRemoteSocketAddress().toString());
+                    ws.send(pgAdmin2MapServer.model.Map.getMapConfigJson(false));
                 }
             }
         }
@@ -225,14 +245,14 @@ public class Pg2MS {
         HttpResponse response = ElementalHttpPost.post(
                 "/" + RequestManager.REQUEST_NEW_PARAMS,
                 Arrays.toString(args).replace("[", "").replace("]", "").replace(" ", "").replace(",", "&"));
-        Pg2MS.log("<< Response: " + response.getStatusLine());
-        Pg2MS.log(EntityUtils.toString(response.getEntity()));
-        Pg2MS.log("==============");
+        Pg2MSJetty.log("<< Response: " + response.getStatusLine());
+        Pg2MSJetty.log(EntityUtils.toString(response.getEntity()));
+        Pg2MSJetty.log("==============");
     }
 
     /**
-     * @param args the command line arguments Client refresh can be triggered
-     * with URL:
+     * @param args the command line arguments.
+     * Client refresh can be triggered with URL of the form:
      * http://localhost:9472/newParams?bindir=/Applications/Dev/pgAdmin3.app/Contents/MacOS&host=&port=5432&database=nicolas&user=nicolas&passwd=&schema=public&table=
      */
     public static void main(String[] args) throws Exception {
@@ -244,9 +264,9 @@ public class Pg2MS {
         try {
             startServer();
         } catch (IOException x) {
-            Pg2MS.log("Another internal server instance already running on " + Pg2MS.serverPort
+            Pg2MSJetty.log("Another internal server instance already running on " + Pg2MS.serverPort
                     + ".\n\tException: " + x.toString());
-            Pg2MS.log("Sending new params to running server:  " + Arrays.toString(args));
+            Pg2MSJetty.log("Sending new params to running server:  " + Arrays.toString(args));
             sendParamsToServer(args);
             System.exit(1);
         }
